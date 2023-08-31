48013d3beb8c7c8127097dd679a2e3684e81f1f2
khoinguyen19k8 <khoinguyen19k8@gmail.com>
2023-08-31 14:42:30 +0200

    AnonymousClasses

---
+++ src/main/scala/com/rockthejvm/part2oop/AnonymousClasses.scala
@@ -0,0 +1,33 @@
+package com.rockthejvm.part2oop
+
+object AnonymousClasses {
+  abstract class Animal {
+    def eat(): Unit
+  }
+
+  class SomeAnimal extends Animal {
+    override def eat(): Unit = println("I'm a weird animal")
+  }
+
+  val someAnimal = new SomeAnimal
+  val someAnimal_v2 = new Animal {
+    override def eat(): Unit = println("I'm a weird animal")
+  }
+
+  /*
+    class AnonymousClasses.Anonclass$1 extends Animal {
+      override def eat(): Unit = println("I'm a weird animal")
+    }
+  */
+  // works for (abstract or not) classes, and traits
+  class Person(name: String) {
+    def sayHi(): Unit = println(s"My name is $name")
+  }
+
+  val jim = new Person("Jim") {
+    override def sayHi(): Unit = println("MY NAME IS JIM!")
+  }
+  def main(args: Array[String]): Unit = {
+
+  }
+}
